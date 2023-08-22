4c4b820e49628e3fd37566463659fd34af220f8d
bee85919 <bee85919@gmail.com>
2023-08-22 15:44:03 +0900

    OOP

--- src/main/scala/OOP/SUV.scala
+++ src/main/scala/OOP/SUV.scala
@@ -4,5 +4,4 @@ package OOP
 class SUV extends Car{
   override def engineStart(): Unit = println("SUV Engine Start")
   override def engineStop(): Unit = println("SUV Engine Stop")
-
 }
--- src/main/scala/OOP/Sedan.scala
+++ src/main/scala/OOP/Sedan.scala
@@ -4,5 +4,4 @@ package OOP
 class Sedan extends Car{
   override def engineStart(): Unit = println("Sedan Engine Start")
   override def engineStop(): Unit = println("Sedan Engine Stop")
-
 }
